fab0aa3b0d3feafb03f937f364b62b18c407f16a
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2026-01-14 16:54:50 +0600

    added reason

---
+++ reason.go
@@ -0,0 +1,23 @@
+package errors
+
+type Reason struct {
+	Type      string     `json:"type"`
+	Info      *string    `json:"info"`
+	Attribute *Attribute `json:"attribute"`
+}
+
+func (r Reason) toHashMap() map[string]interface{} {
+	d := make(map[string]interface{})
+
+	d["type"] = r.Type
+
+	if r.Info != nil {
+		d["info"] = *r.Info
+	}
+
+	if r.Attribute != nil {
+		d["attribute"] = r.Attribute.toHashMap()
+	}
+
+	return d
+}
